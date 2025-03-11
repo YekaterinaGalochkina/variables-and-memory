7ed3b4812ed8e380e58d5c12cbcfe2f3da716600
Yekaterina Galochkina <galocichina.ecaterina@gmail.com>
2025-03-11 14:32:36 -0700

    activity work/ scope challenges

--- challenges/scope_challenges.py
+++ challenges/scope_challenges.py
@@ -1,23 +1,23 @@
 def challenge_01():
-    # 1
+    # 1 local
     outer_value = 42
-
     print(f"The value from outside is: {outer_value}")
+    
 
 def challenge_02():
-    # 2
+    # 2 global
     print(f"The value from outside is: {outer_value}")
+
 
 def challenge_03():
-    # 3
+    # 3 The variable is not declared, so it gives an error
     print(f"The value from outside is: {inner_value}")
-
     inner_value = 42
 
-def challenge_04():
-    # 4
-    print(f"The value from outside is: {outer_value}")
 
+def challenge_04():
+    # 4 Same variable name as global, declared locally after variable was used, -> error
+    print(f"The value from outside is: {outer_value}")
     outer_value = 42
 
 outer_value = 1729
